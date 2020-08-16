7fd84c3c07bdae09a31f567c07d65290e47df007
orenjiakira <orenjiakira@gmail.com>
2020-08-16 10:12:10 -0300

    Modify main to draw triangle

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 
+#include <GL/glew.h>
+
 #include <iostream>
 
+#include "GLFW/glfw3.h"
 #include "front_end/front_end_system.h"
 #include "front_end/modules/events.h"
 #include "front_end/modules/graphics.h"
@@ -11,6 +14,54 @@ using SargassoEngine::FrontEnd::Modules::Events;
 using SargassoEngine::FrontEnd::Modules::Graphics;
 using SargassoEngine::FrontEnd::Modules::Time;
 
+static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f, -1.0f, 0.0f,
+
+    1.0f,  -1.0f, 0.0f,
+
+    0.0f,  1.0f,  0.0f,
+};
+
+GLuint create_vertex_array_object() {
+  GLuint vertex_array_id;
+  glGenVertexArrays(1, &vertex_array_id);
+  glBindVertexArray(vertex_array_id);
+  return vertex_array_id;
+}
+
+GLuint create_triangle_buffer() {
+  // This will identify our vertex buffer
+  GLuint vertex_buffer;
+  // Generate 1 buffer, put the resulting identifier in vertex_buffer
+  glGenBuffers(1, &vertex_buffer);
+  // The following commands will talk about our 'vertex_buffer' buffer
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  // Give our vertices to OpenGL.
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+               g_vertex_buffer_data, GL_STATIC_DRAW);
+  return vertex_buffer;
+}
+
+void render_triangle(const GLuint &vertex_buffer) {
+  // 1st attribute buffer : vertices
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glVertexAttribPointer(0,  // attribute 0. No particular reason for 0, but must
+                            // match the layout in the shader.
+                        3,  // size
+                        GL_FLOAT,  // type
+                        GL_FALSE,  // normalized?
+                        0,         // stride
+                        (void *)0  // array buffer offset
+  );
+
+  // Draw the triangle !
+
+  // Starting from vertex 0; 3 vertices total -> 1 triangle
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDisableVertexAttribArray(0);
+}
+
 int main() {
   std::cout << "Hello world" << std::endl;
 
@@ -18,6 +69,9 @@ int main() {
   if (!front_end.is_initialized()) {
     return -1;
   }
+
+  const GLuint vao = create_vertex_array_object();
+  const GLuint buffer = create_triangle_buffer();
 
   std::cout << "Init successful!" << std::endl;
 
@@ -35,6 +89,8 @@ int main() {
 
     frame_number++;
     graphics.render();
+    render_triangle(buffer);
+
     events.poll_events();
 
     time.end_frame();
