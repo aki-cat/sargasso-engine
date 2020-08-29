a613a8fe6e1be8c58f5950c28726d99128fdc177
orenjiakira <orenjiakira@gmail.com>
2020-08-29 18:56:28 -0300

    Use variadic templates for logs and string format

--- src/common/log.h
+++ src/common/log.h
@@ -1,56 +1,12 @@
 #ifndef SARGASSO_ENGINE_LOG_H
 #define SARGASSO_ENGINE_LOG_H
 
+#include "common/format.h"
+
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <utility>
 
 namespace SargassoEngine {
 namespace Common {
-
-template <typename... Args>
-std::string format(const std::string& fmt, Args... args);
-
-template <typename... Args>
-struct Format {};
-
-template <>
-struct Format<> {
-    static std::string format(const std::string& fmt);
-};
-
-template <typename T, typename... Args>
-struct Format<T, Args...> {
-    static std::string format(const std::string& fmt, T value, Args... args);
-};
-
-inline std::string Format<>::format(const std::string& fmt) { return fmt; }
-
-template <typename T, typename... Args>
-inline std::string Format<T, Args...>::format(const std::string& fmt, T value, Args... args) {
-    size_t idx = fmt.find('%');
-    char next = 0;
-    if (idx != std::string::npos && fmt.size() > idx + 1) {
-        next = fmt[idx + 1];
-    }
-    if (idx != std::string::npos && next != '%') {
-        std::ostringstream os;
-        os << fmt.substr(0, idx) << value << fmt.substr(idx + 1);
-        return Format<Args...>::format(os.str(), args...);
-    }
-    if (next == '%') {
-        std::string before = fmt.substr(0, idx + 1);
-        std::string after = Format<T, Args...>::format(fmt.substr(idx + 2), value, args...);
-        return before + after;
-    }
-    return fmt;
-}
-
-template <typename... Args>
-inline std::string format(const std::string& fmt, Args... args) {
-    return Format<Args...>::format(fmt, args...);
-}
 
 inline void log(const std::string& str) { std::cout << str << std::endl; }
 
