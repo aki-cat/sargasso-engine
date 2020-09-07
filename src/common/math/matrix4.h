2f1e9e01db1636a579b315b13433fcd52c5c9df0
orenjiakira <orenjiakira@gmail.com>
2020-09-07 20:28:54 -0300

    Implement most matrix operations

--- src/common/math/matrix4.h
+++ src/common/math/matrix4.h
@@ -27,8 +27,6 @@ class Mat4 {
     Mat4 rotated(const Vec3& axis, const float angle) const;
     Mat4& rotate(const Vec3& axis, const float angle);
 
-    Mat4 multiplied(const Vec3& v) const;
-    Mat4& multiply(const Vec3& v);
     std::string to_string() const;
 
     operator std::string();
@@ -47,11 +45,14 @@ Mat4 operator-(const Mat4& a, const Mat4& b);
 Mat4 operator-(const Mat4& m);
 
 Mat4 operator*(const Mat4& a, const Mat4& b);
-Mat4 operator*(const Mat4& m, const Vec3& v);
+Vec3 operator*(const Mat4& m, const Vec3& v);
+Mat4 operator*(const Mat4& m, const float a);
+Mat4 operator*(const float a, const Mat4& m);
 
-Mat4& operator+=(const Mat4& a, const Mat4& b);
-Mat4& operator-=(const Mat4& a, const Mat4& b);
-Mat4& operator*=(const Mat4& a, const Mat4& b);
+Mat4& operator+=(Mat4& a, const Mat4& b);
+Mat4& operator-=(Mat4& a, const Mat4& b);
+Mat4& operator*=(Mat4& a, const Mat4& b);
+Mat4& operator*=(Mat4& m, const float a);
 
 /*
 
@@ -76,46 +77,37 @@ const Mat4& Mat4::zero = Mat4(Points16{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 // Methods
 
 Mat4 Mat4::translated(const Vec3& v) const {
-    Mat4 m{*this};
+    Mat4 m = Mat4::identity;
     m[3] += v.x;
     m[7] += v.y;
     m[11] += v.z;
-    return m;
+    return (*this) * m;
 }
 
 Mat4& Mat4::translate(const Vec3& v) {
-    _points[3] += v.x;
-    _points[7] += v.y;
-    _points[11] += v.z;
-    return *this;
+    Mat4 m = Mat4::identity;
+    m[3] += v.x;
+    m[7] += v.y;
+    m[11] += v.z;
+    return (*this) *= m;
 }
 
-Mat4 Mat4::scaled(const float a) const {
-    // TBD
-    return Mat4();
-}
+Mat4 Mat4::scaled(const float a) const { return (*this) * a; }
 
 Mat4& Mat4::scale(const float a) {
-    // TBD
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        _points[i] *= a;
+    }
     return *this;
 }
 
 Mat4 Mat4::rotated(const Vec3& axis, const float angle) const {
     // TBD
-    return Mat4();
+    Mat4 m{*this};
+    return m;
 }
 
 Mat4& Mat4::rotate(const Vec3& axis, const float angle) {
-    // TBD
-    return *this;
-}
-
-Mat4 Mat4::multiplied(const Vec3& v) const {
-    // TBD
-    return Mat4();
-}
-
-Mat4& Mat4::multiply(const Vec3& v) {
     // TBD
     return *this;
 }
@@ -138,45 +130,116 @@ float& Mat4::operator[](const uint32_t n) { return _points[n]; }
 // Imutable operators
 
 Mat4 operator+(const Mat4& a, const Mat4& b) {
-    // TBD
-    return Mat4();
+    Mat4 m{};
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        m[i] = a[i] + b[i];
+    }
+    return m;
 }
 
 Mat4 operator-(const Mat4& a, const Mat4& b) {
-    // TBD
-    return Mat4();
+    Mat4 m{};
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        m[i] = a[i] - b[i];
+    }
+    return m;
 }
 
 Mat4 operator*(const Mat4& a, const Mat4& b) {
-    // TBD
-    return Mat4();
+    return Mat4{Points16{a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12],
+                         a[0] * b[1] + a[1] * b[5] + a[2] * b[9] + a[3] * b[13],
+                         a[0] * b[2] + a[1] * b[6] + a[2] * b[10] + a[3] * b[14],
+                         a[0] * b[3] + a[1] * b[7] + a[2] * b[11] + a[3] * b[15],
+                         a[4] * b[0] + a[5] * b[4] + a[6] * b[8] + a[7] * b[12],
+                         a[4] * b[1] + a[5] * b[5] + a[6] * b[9] + a[7] * b[13],
+                         a[4] * b[2] + a[5] * b[6] + a[6] * b[10] + a[7] * b[14],
+                         a[4] * b[3] + a[5] * b[7] + a[6] * b[11] + a[7] * b[15],
+                         a[8] * b[0] + a[9] * b[4] + a[10] * b[8] + a[11] * b[12],
+                         a[8] * b[1] + a[9] * b[5] + a[10] * b[9] + a[11] * b[13],
+                         a[8] * b[2] + a[9] * b[6] + a[10] * b[10] + a[11] * b[14],
+                         a[8] * b[3] + a[9] * b[7] + a[10] * b[11] + a[11] * b[15],
+                         a[12] * b[0] + a[13] * b[4] + a[14] * b[8] + a[15] * b[12],
+                         a[12] * b[1] + a[13] * b[5] + a[14] * b[9] + a[15] * b[13],
+                         a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14],
+                         a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15]}};
 }
 
-Mat4 operator*(const Mat4& m, const Vec3& v) {
+// We always assume vector is susceptible to translations (w = 1)
+Vec3 operator*(const Mat4& m, const Vec3& v) {
     // TBD
-    return Mat4();
+    return Vec3{m[0] * v.x + m[1] * v.y + m[2] * v.z + m[3],
+                m[4] * v.x + m[5] * v.y + m[6] * v.z + m[7],
+                m[8] * v.x + m[9] * v.y + m[10] * v.z + m[11]};
+}
+
+Mat4 operator*(const Mat4& m, const float a) {
+    Mat4 n{};
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        n[i] = m[i] * a;
+    }
+    return n;
+}
+
+Mat4 operator*(const float a, const Mat4& m) {
+    Mat4 n{};
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        n[i] = m[i] * a;
+    }
+    return n;
 }
 
 Mat4 operator-(const Mat4& m) {
-    // TBD
-    return Mat4();
+    Mat4 n{};
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        n[i] = -m[i];
+    }
+    return n;
 }
 
 // Mutable operators
 
 Mat4& operator+=(Mat4& a, const Mat4& b) {
-    // TBD
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        a[i] += b[i];
+    }
     return a;
 }
 
 Mat4& operator-=(Mat4& a, const Mat4& b) {
-    // TBD
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        a[i] -= b[i];
+    }
     return a;
 }
 
 Mat4& operator*=(Mat4& a, const Mat4& b) {
-    // TBD
+    Mat4 m{Points16{a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12],
+                    a[0] * b[1] + a[1] * b[5] + a[2] * b[9] + a[3] * b[13],
+                    a[0] * b[2] + a[1] * b[6] + a[2] * b[10] + a[3] * b[14],
+                    a[0] * b[3] + a[1] * b[7] + a[2] * b[11] + a[3] * b[15],
+                    a[4] * b[0] + a[5] * b[4] + a[6] * b[8] + a[7] * b[12],
+                    a[4] * b[1] + a[5] * b[5] + a[6] * b[9] + a[7] * b[13],
+                    a[4] * b[2] + a[5] * b[6] + a[6] * b[10] + a[7] * b[14],
+                    a[4] * b[3] + a[5] * b[7] + a[6] * b[11] + a[7] * b[15],
+                    a[8] * b[0] + a[9] * b[4] + a[10] * b[8] + a[11] * b[12],
+                    a[8] * b[1] + a[9] * b[5] + a[10] * b[9] + a[11] * b[13],
+                    a[8] * b[2] + a[9] * b[6] + a[10] * b[10] + a[11] * b[14],
+                    a[8] * b[3] + a[9] * b[7] + a[10] * b[11] + a[11] * b[15],
+                    a[12] * b[0] + a[13] * b[4] + a[14] * b[8] + a[15] * b[12],
+                    a[12] * b[1] + a[13] * b[5] + a[14] * b[9] + a[15] * b[13],
+                    a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14],
+                    a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15]}};
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        a[i] = m[i];
+    }
     return a;
+}
+
+Mat4& operator*=(Mat4& m, const float a) {
+    for (uint32_t i = 0; i < Points16::len; i++) {
+        m[i] *= a;
+    }
+    return m;
 }
 
 }  // namespace Math
