4e6ca2639189e69fd169f9bcc1370ff399b6db0c
aki-cat <orenjiakira@gmail.com>
2022-06-12 03:15:40 -0300

    Add more complexity to reference class

--- src/sargasso/common/reference.h
+++ src/sargasso/common/reference.h
@@ -7,10 +7,14 @@
 namespace sargasso {
 namespace common {
 
+class Counter;
+
 template <typename T>
 class Reference {
    public:
-    explicit Reference(const uint64_t id, T& data);
+    explicit Reference(const uint64_t id, T* data);
+    Reference(const Reference&);  // copy
+    ~Reference();
 
     T* operator->();
     const T* operator->() const;
@@ -18,21 +22,93 @@ class Reference {
     const uint64_t get_id() const;
 
     bool is_alive() const;
+    bool is_dead() const;
+    void destroy();
 
    private:
     const uint64_t _id;
-    T& _data;
+    Counter* _ref_count;
+    T* _ref;
+};
+
+class Counter {
+   public:
+    Counter() = default;
+
+    void reset() {
+        _count = 0;
+    }
+
+    Counter& operator++() {
+        _count++;
+        return *this;
+    }
+
+    Counter& operator--() {
+        if (_count - 1 != UINT64_MAX) {
+            _count--;
+        } else {
+            _count = 0;
+        }
+        return *this;
+    }
+
+    bool operator==(uint64_t n) const {
+        return _count == n;
+    }
+
+    bool operator>(uint64_t n) const {
+        return _count > n;
+    }
+
+    bool operator>=(uint64_t n) const {
+        return _count >= n;
+    }
+
+    bool operator<=(uint64_t n) const {
+        return _count <= n;
+    }
+
+    bool operator<(uint64_t n) const {
+        return _count <= n;
+    }
+
+   private:
+    uint64_t _count;
 };
 
 template <typename T>
-Reference<T>::Reference(const uint64_t id, T& data) : _id(id), _data(data) {}
+Reference<T>::Reference(const uint64_t id, T* data) : _id(id), _ref(data) {
+    _ref_count = new Counter();
+    if (_ref) {
+        ++(*_ref_count);
+    }
+}
+
+template <typename T>
+Reference<T>::Reference(const Reference<T>& original) : _id(original._id) {
+    _ref = original._ref;
+    _ref_count = original._ref_count;
+    ++(*_ref_count);
+}
+
+template <typename T>
+Reference<T>::~Reference() {
+    --(*_ref_count);
+
+    if (!is_alive()) {
+
+        delete _ref;
+        delete _ref_count;
+    }
+}
 
 template <typename T>
 T* Reference<T>::operator->() {
     if (!is_alive()) {
         throw std::runtime_error("Attempt to access dead reference.");
     }
-    return &_data;
+    return _ref;
 }
 
 template <typename T>
@@ -40,7 +116,7 @@ const T* Reference<T>::operator->() const {
     if (!is_alive()) {
         throw std::runtime_error("Attempt to access dead reference.");
     }
-    return _data;
+    return _ref;
 }
 
 template <typename T>
@@ -50,8 +126,18 @@ const uint64_t Reference<T>::get_id() const {
 
 template <typename T>
 bool Reference<T>::is_alive() const {
-    // TBD
-    return true;
+    return !is_dead();
+}
+
+template <typename T>
+bool Reference<T>::is_dead() const {
+    return !_ref || (*_ref_count) == 0;
+}
+
+template <typename T>
+void Reference<T>::destroy() {
+    // doesn't immediately destroy, but object is destroyed as soon as the Reference is destroyed.
+    _ref_count->reset();
 }
 
 }  // namespace common
--- src/tests/spec/reference.spec.cxx
+++ src/tests/spec/reference.spec.cxx
@@ -11,32 +11,50 @@ struct Example {
 
 DESCRIBE_CLASS(Reference<Example>) {
     DESCRIBE_TEST(operator->, Accessed public member, Return expected value) {
-        Example foo = {3, 4};
+        Example* foo = new Example{3, 4};
         auto foo_reference = Reference<Example>(0, foo);
         ASSERT_ARE_EQUAL(foo_reference->x, 3);
     };
 
     DESCRIBE_TEST(operator->, Accessed public member, Return same reference value) {
-        Example foo = {3, 4};
-        auto foo_reference = Reference<Example>(0, foo);
-        ASSERT_ARE_SAME(foo_reference->x, foo.x);
+        Example* foo = new Example{3, 4};
+        auto foo_reference = Reference<Example>(1, foo);
+        ASSERT_ARE_SAME(foo_reference->x, foo->x);
     };
 
     DESCRIBE_TEST(operator->, Original data changed, Update data inside reference) {
-        Example foo = {3, 4};
-        auto foo_reference = Reference<Example>(0, foo);
-        foo.x = 99;
+        Example* foo = new Example{3, 4};
+        auto foo_reference = Reference<Example>(2, foo);
+        foo->x = 99;
         ASSERT_ARE_EQUAL(foo_reference->x, 99);
     };
 
     DESCRIBE_TEST(operator->, Changed data via reference, Update original data) {
-        Example foo = {3, 4};
-        auto foo_reference = Reference<Example>(0, foo);
+        Example* foo = new Example{3, 4};
+        auto foo_reference = Reference<Example>(3, foo);
         foo_reference->x = 99;
-        ASSERT_ARE_EQUAL(foo.x, 99);
+        ASSERT_ARE_EQUAL(foo->x, 99);
     };
 
-    DESCRIBE_TEST(std::is_trivially_copyable, Checked by compiler, Return true) {
-        ASSERT_IS_TRUE(std::is_trivially_copyable<Reference<Example>>());
+    DESCRIBE_TEST(destroy(), Method called, Destroy object) {
+        Example* foo = new Example{3, 4};
+        auto foo_reference = Reference<Example>(4, foo);
+        const Reference<Example> second_ref = foo_reference;
+
+        foo_reference.destroy();
+
+        ASSERT_IS_TRUE(foo_reference.is_dead());
     };
+
+    DESCRIBE_TEST(std::is_trivially_copy_assignable, Checked by compiler, Return false) {
+        ASSERT_IS_FALSE(std::is_trivially_copy_assignable<Reference<Example>>());
+    };
+
+    DESCRIBE_TEST(operator--, Subtract at zero, Underflow does not happen) {
+        sargasso::common::Counter foo{};
+        --foo;
+        ASSERT_IS_TRUE(foo == 0);
+    };
+
+
 }
